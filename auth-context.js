f8f337b15f4e8e7161ffb699912503273389d676
kiran chavan <125879292+Kiranchavan05@users.noreply.github.com>
2023-05-19 07:55:16 +0530

    ecommerce task21 auto log out

--- auth-context.js
+++ auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 const AuthContext = React.createContext({
   token: "",
@@ -10,17 +10,57 @@ const AuthContext = React.createContext({
  export const AuthContextProvider = (props) => {
   const initialToken=localStorage.getItem('token')
   const [token, setToken] = useState(initialToken);
+  const [logoutTimer, setLogoutTimer]= useState(null);
 
   const userIsLoggedIn = !!token;
+
+
+  // const checkLogout=()=>{
+  //   const loginTime=localStorage.getTime('loginTime')
+  //   if(loginTime) {
+  //     const currentTime=new Date().getTime();
+  //     const timeSinceLogin=currentTime-parsenInt(loginTime,10)
+  //     const fiveMinutesTnMilliseconds=5* 60 * 1000;
+
+  //     if (timeSinceLogin>=fiveMinutesTnMilliseconds) {
+  //       logout();
+  //     }
+  //   };
+  // };
+
+  const calculateRemainingTime = (expirytime) => {
+    const currentTime = new Date().getTime();
+    const remainingTime = expirytime - currentTime;
+    return remainingTime;
+  };
 
   const logInHandler = (token) => {
     setToken(token);
     localStorage.setItem('token', token)
+
+    const expireTime = new Date().getTime() + 5 * 60 * 1000;
+    localStorage.setItem("expiry", expireTime.toString());
+
+    const remainingTime = calculateRemainingTime(expireTime);
+
+    setLogoutTimer(setTimeout(logoutHandler, remainingTime));
   };
+
+  useEffect(() => {
+    if (token && logoutTimer) {
+      clearInterval(logoutTimer);
+    }
+    if (token) {
+      const expirationTime = Number(localStorage.getItem("expiry"));
+      const remainingTime = calculateRemainingTime(expirationTime);
+      setLogoutTimer(setTimeout(logoutHandler, remainingTime));
+    }
+  }, [token]);
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token')
+    localStorage.removeItem("expiry");
 
   };
 
